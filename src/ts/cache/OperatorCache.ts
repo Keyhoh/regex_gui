4d56353e4259fadbe111778fd85a29fa431bd7a6
Keyhoh <iheyhoh@gmail.com>
2018-09-22 13:38:54 +0900

    add page files and setting files

--- src/ts/cache/OperatorCache.ts
+++ src/ts/cache/OperatorCache.ts
@@ -6,7 +6,7 @@ import {GrouperImpl} from "../app/impl/GrouperImpl";
 export class OperatorCache {
     public static operators: Map<string, GrouperImpl>;
 
-    public static initialize(){
+    public static initialize() {
         let operators = new Map<string, GrouperImpl>();
         json.Groupers.forEach((value) => {
             operators.set(value.kind, new GrouperImpl(<Grouper> value));
